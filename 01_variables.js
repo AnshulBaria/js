e2e76efae8eecc3ac74cd4ef70f2461d22bf8413
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-03-02 23:54:31 +0530

    JS

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId =12345 ;
+let userName = "anshul";
+var emailID = "anshul@gmail.com"
+
+// accountId = 54321; // not valid
+
+userName = "john_doe";
+emailID = "abc@gmail.com";
+
+console.log(accountId);
+console.log(userName);
+console.log(emailID);
+
+console.table({accountId, userName, emailID});
---
+++ 02_datatypes.js
@@ -0,0 +1,6 @@
+let score = 99
+console.log(typeof score);
+
+let ValueInNUmber=score
+console.log(typeof ValueInNUmber);
+console.log(ValueInNUmber);
