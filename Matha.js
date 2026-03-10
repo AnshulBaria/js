9938b7ebfc9b4db63538aa522379bf27ba9f86a8
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-03-10 20:51:51 +0000

    Math and num

---
+++ Matha.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const Bal = new Number(100)
+// console.log(Bal);
+
+// console.log(Bal.toString());
+// console.log(Bal.toFixed(2));
+
+const OtherNumber= 203.46132
+// console.log(OtherNumber.toPrecision(4));
+const hundreads = 100000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+//---------------------------------------------------
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,5,9));
+// console.log(Math.max(4,5,9));
+console.log((Math.random()*10)+1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+
+
+
+
