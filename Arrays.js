76577d5f1d336c1785a5c52171bc738708f2e906
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-03-11 15:50:01 +0000

    arrays

---
+++ Arrays.js
@@ -0,0 +1,39 @@
+const my_Arr = [2,3,4,5,6,7]
+// console.log(my_Arr);
+// console.log(my_Arr[2]);
+const MYarr2 = new Array (2,2,2,2,2,)
+// console.log(MYarr2);
+my_Arr.push(8)
+// console.log(my_Arr);
+// my_Arr.pop()
+// console.log(my_Arr);
+// my_Arr.unshift(0)
+// console.log(my_Arr);
+// my_Arr.shift(my_Arr)
+// console.log(my_Arr);
+// console.log(my_Arr.includes(9));
+// console.log(my_Arr);
+// console.log(my_Arr.indexOf(5));
+
+const newArr = MYarr2.join()
+// console.log(newArr); 
+// console.log(typeof newArr);
+
+// console.log(MYarr2);
+
+console.log("a",MYarr2);
+const myn1 = my_Arr.slice(1,3)
+console.log(myn1);
+
+const myn2 = MYarr2.splice(1,3)
+console.log(myn2);
+console.log(myn1);
+
+
+
+
+
+
+
+
+
