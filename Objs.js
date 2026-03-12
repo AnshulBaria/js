7053a2e3326a06d412b7fd114ea77b49e18da8cf
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-03-12 21:41:21 +0000

    Fix function syntax in Objs.js

---
+++ Objs.js
@@ -0,0 +1,38 @@
+// singleton
+// obj literals
+//Object.create
+const MySym = Symbol("Key1")
+
+const JsUser = {
+    Name: "Anshul",
+    "Full_Name":"Baria",
+    [MySym]:  "MyKey!",
+    Age: 18,
+    loc: "Vadodara",
+    email: "anshulbaria192@gmail.com",
+    isloggedIN: false,
+    lastLoginDays:["mon","satur"]
+}
+// console.log(JsUser["email"]);
+// console.log(JsUser.Full_Name);
+// console.log(JsUser[MySym]);
+// console.log(typeof MySym);
+
+JsUser.email = "ansh@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "anshbaria@gmail.com"
+// console.log(JsUser["email"]);
+
+JsUser.greeting = function() {
+     console.log(`Hello JS User${this.Name}`);
+}
+
+JsUser.greetingTwo = function() {
+     console.log(`Hello JS User ${this. Name}`);
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo);
+console.log(JsUser.greetingTwo());
+
+ 
---
+++ js
@@ -0,0 +1,1 @@
+anshulbaria192@gmail.com
