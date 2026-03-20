ce19402cfaee3b0bb290dca38abde1ab4d5e3fa8
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-03-20 04:14:20 +0000

    finish part one

--- 01_Obj.js
+++ 01_Obj.js
@@ -10,18 +10,35 @@ const JsUser = {
     email: 'anshu@example.com',
     isloggedIn: true,
     lastLoginDays: ['Monday', 'Tuesday', 'Wednesday'],
-    
+    greeting: function() {
+        console.log(`Hello, my name is ${this.name}`);
+    },
+    greetingTwo: function() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
 };
+console.log(JsUser.greetingTwo());
+
+
 // console.log(JsUser.name);
 // console.log(JsUser.isloggedIn);
 // console.log(JsUser.lastLoginDays);
+// console.log(JsUser["fullName"]);
+
 
 // console.log(JsUser["email"]);
 // console.log(JsUser[MySym]);
 // console.log(typeof JsUser[MySym]);
+// console.log(JsUser.email);
+Object.freeze(JsUser) // freeze the obj to prevent modification
 
-Object.freeze(JsUser) 
 JsUser.email = "newemail@example.com"
 // console.log(JsUser.email);
 
-console.log(JsUser.email);
+JsUser.greeting = function() {
+    console.log(`Hello, my name is ${this.name}`);
+}
+
+
+// console.log(JsUser.greeting);
+// console.log(JsUser.greeting());
