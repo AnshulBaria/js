45593a44606fc580fe845f4cbf37d37c62142283
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-03-25 10:47:35 +0000

    scopes

--- 02_scopes.js
+++ 02_scopes.js
@@ -1,16 +1,48 @@
-let a =10 
+let a = 10
 const b = 20
-var c =600
+var c = 600
 
-if (true){
+if (true) {
     let a = 100
     const b = 200
-    var c = 300 
-    console.log("inside block ",a); // this will print 100 because of block scope 
+    var c = 300
+    console.log("inside block ", a); // this will print 100 because of block scope 
 }
-console.log(a); // not accessible because of block scope it will print global a which is 10
+// console.log(a); // not accessible because of block scope it will print global a which is 10
 // console.log(b); not accessible because of block scope it will print global b which is 20 
 // console.log(c); this will print 300 because var is function scoped and not block scoped
 
+function one() {
+    const userName = "anshul"
+    function two() {
+        const rollNumder = 123
+        // console.log(userName);
+
+    }
+    // console.log(rollNumder);
+
+    two()
+}
+one()
 
 
+if (true) {
+    const userName = "anshul"
+    if (userName === "anshul") {
+        const rollNumder = 123
+        // console.log(userName +  rollNumder);
+    }
+    // console.log(rollNumder);
+}
+// console.log(userName);
+
+// ======================= //
+console.log(addone(3));
+function addone (value) {
+    return value + 1
+}
+
+// console.log(addTwo(3)); due to hoisting this will throw an error because addTwo is a function expression and not a function declaration
+const addTwo = function(value) {
+    return value + 2
+}
