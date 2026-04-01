0b4fb4cbab35bf4ffedd343762872104dbad97ee
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-04-01 10:22:34 +0000

    iife

---
+++ 04_iife.js
@@ -0,0 +1,8 @@
+(function dataBase (){
+    console.log(`Data base initialized`);
+    
+})();
+
+(function dataBase01 (name) {
+    console.log(`Data base 01 initialized with name: ${name}`);
+})("John");
