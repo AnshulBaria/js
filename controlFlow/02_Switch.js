3993c2f793589ace9faba2f6f29d9bb7db6c500b
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-04-07 03:59:32 +0000

    Switch

---
+++ controlFlow/02_Switch.js
@@ -0,0 +1,17 @@
+const day = "sunday";
+switch (day) {
+    // case "sunday":
+        // console.log("it's sunday");
+        // break;
+    case "monday":
+        console.log("it's monday");
+        break;
+        case "tuesday":
+            console.log("it's tuesday");
+            break;
+        case "wednesday":
+            console.log("it's wednesday");
+            break;
+        default:
+            console.log("it's some other day");
+}
