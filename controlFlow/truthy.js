9d5c457bd459c1f265ad1a5934375b5a56d372c7
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-04-08 10:00:27 +0000

    truthy

--- controlFlow/truthy.js
+++ controlFlow/truthy.js
@@ -1,9 +1,9 @@
 const userEmail = "a@anshul.com"
 // if (userEmail)
-if (userEmail){
+if (userEmail) {
     console.log("got user email");
-    
-}else{
+
+} else {
     console.log("user email not found");
 }
 
@@ -28,3 +28,19 @@ if (userEmail){
 // 1
 // -1
 // BigInt(1n)
+
+// nullish coalescing operator (??) null , undefined
+// const userEmail = null
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// console.log(val1);
+
+// val1 = null ?? 10 ?? 5
+// console.log(val1);
+
+// ternary operator
+// Condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("I will buy ice tea") : console.log("I will not buy ice tea");
