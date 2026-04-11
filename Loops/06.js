dd991ba03e8de21fa261b2e9deac00e53258c647
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-04-11 20:14:59 +0000

    forEach

---
+++ Loops/06.js
@@ -0,0 +1,22 @@
+const coding = ["JavaScript","Python","Java","C++"];
+const values =coding.forEach((val)=>{
+    // console.log(val);
+    // return val;
+})
+// console.log(values); // undefined
+const Nums = [1,2,3,4,5];
+// // const Newnums = Nums.filter( (num)=> num > 2 )
+// // console.log(Newnums);
+
+// const Newnums = Nums.filter( (num)=> {
+//     return num > 2;
+// } );
+
+// const newNums =[]
+// Nums.forEach((num)=>{
+//     if(num > 2){
+//         newNums.push(num);
+//     }
+// })
+// console.log(newNums);
+
