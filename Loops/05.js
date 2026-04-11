e5b0c7a441972e86d60eacde4dc73e6b1d3391eb
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-04-11 13:24:30 +0000

    ForEach

---
+++ Loops/05.js
@@ -0,0 +1,18 @@
+// const Names = ["js","python","java","cpp"];
+// Names.forEach(function(val){
+//     console.log(val);
+// })
+
+const coding = ["js","python","java","cpp"];
+coding.forEach((val)=>{
+    // console.log(val);
+})
+
+function print(val){
+    // console.log(val);
+}
+coding.forEach(print);
+
+coding.forEach((val,index,array)=>{
+    // console.log(`${index} : ${val} ${array}`);
+})
