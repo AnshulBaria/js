a0d49f87de6866ef27f90f06dab4acca748e0f5b
AnshulBaria <99167142+AnshulBaria@users.noreply.github.com>
2026-04-11 19:53:28 +0000

    forEach02

--- Loops/05.js
+++ Loops/05.js
@@ -16,3 +16,17 @@ coding.forEach(print);
 coding.forEach((val,index,array)=>{
     // console.log(`${index} : ${val} ${array}`);
 })
+
+const users = [
+    {language:"javascript",
+    languageFileExtension:".js"},
+    {language:"java",
+    languageFileExtension:".java"},
+    {language:"python",
+    languageFileExtension:".py"},
+    ];
+users.forEach((item)=>{
+    console.log(item.language);
+    console.log(item.languageFileExtension);
+})
+
