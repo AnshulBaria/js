c00883d2af06e820d16cb77f6cc962fe1de7ab94
Anshul Baria <99167142+AnshulBaria@users.noreply.github.com>
2026-07-22 10:54:18 +0000

    basic

---
+++ basic.js
@@ -0,0 +1,15 @@
+// stack and heap 
+// let firstname = "anshul";
+// let anotheranme = firstname
+// anotheranme = "neel";
+// console.log(anotheranme);
+// console.log(firstname);
+
+let Userone ={
+    Name:"anshul",
+    email :"anshulbaria192@gmail.com",
+    number : 664810831
+}
+let user2 = Userone
+user2.number =982483461
+console.log(user2.number)
